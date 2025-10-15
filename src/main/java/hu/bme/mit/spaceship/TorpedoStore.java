71abce265faf453e6e5f1eb07f1c49f1e3797991
unknown <190916@bme.hu>
2025-10-15 15:05:42 +0200

    Adding emojis for better reading experience

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
-  private double FAILURE_RATE = 0.0; //NOSONAR
+  private double FAILURE_RATE = 0.0; //NOSONAR 😊
 
   private int torpedoCount = 0;
   private Random generator;
@@ -18,33 +18,34 @@ public class TorpedoStore {
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
     generator = new Random();
-    // update failure rate if it was specified in an environment variable
+    
+    // update failure rate if it was specified in an environment variable 😊
     String failureEnv = System.getenv("IVT_RATE");
     if (failureEnv != null){
       try {
-        FAILURE_RATE = Double.parseDouble(failureEnv);
+        FAILURE_RATE = Double.parseDouble(failureEnv); 
       } catch (NumberFormatException nfe) {
-        FAILURE_RATE = 0.0;
+        FAILURE_RATE = 0.0; 
       }
     }
   }
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos"); // Invalid number of torpedos 😞
     }
 
     boolean success = false;
 
-    // simulate random overheating of the launcher bay which prevents firing
+    // simulate random overheating of the launcher bay which prevents firing 😵
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
-      // successful firing
+      // successful firing 😎
       this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
-      // simulated failure
+      // simulated failure 😔
       success = false;
     }
 
@@ -52,10 +53,10 @@ public class TorpedoStore {
   }
 
   public boolean isEmpty(){
-    return this.torpedoCount <= 0;
+    return this.torpedoCount <= 0; // Is the torpedo store empty? 😬
   }
 
   public int getTorpedoCount() {
-    return this.torpedoCount;
+    return this.torpedoCount; // Return the number of remaining torpedos 💥
   }
 }
